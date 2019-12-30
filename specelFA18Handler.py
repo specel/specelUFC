2ac9874d74f53f1d0f623f67f33585c1376c8fa3
Michal Plichta <michal.plichta@nokia.com>
2019-12-30 22:30:43 +0100

    simplify setData form FA-18

--- specelFA18Handler.py
+++ specelFA18Handler.py
@@ -26,22 +26,22 @@ class FA18Handler(AircraftHandler):
         self.OptionCueing5=""
         self.FuelTotal=""
         
-        self.bufferScratchpadString1Display = StringBuffer(self.g13.parser, 0x744e  , 2, lambda s: self.setData(1,s))
-        self.bufferScratchpadString2Display = StringBuffer(self.g13.parser, 0x7450  , 2, lambda s: self.setData(2,s))
-        self.bufferScratchpadNumberDisplay = StringBuffer(self.g13.parser, 0x7446  , 8, lambda s: self.setData(3,s))
-        self.bufferOptionDisplay1 = StringBuffer(self.g13.parser, 0x7432    , 4, lambda s: self.setData(11,s))
-        self.bufferOptionDisplay2 = StringBuffer(self.g13.parser, 0x7436    , 4, lambda s: self.setData(12,s))
-        self.bufferOptionDisplay3 = StringBuffer(self.g13.parser, 0x743a    , 4, lambda s: self.setData(13,s))
-        self.bufferOptionDisplay4 = StringBuffer(self.g13.parser, 0x743e   , 4, lambda s: self.setData(14,s))
-        self.bufferOptionDisplay5 = StringBuffer(self.g13.parser, 0x7442   , 4, lambda s: self.setData(15,s))
-        self.bufferCOMM1Display = StringBuffer(self.g13.parser, 0x7424   , 2, lambda s: self.setData(21,s))
-        self.bufferCOMM2Display = StringBuffer(self.g13.parser, 0x7426   , 2, lambda s: self.setData(22,s))
-        self.bufferOptionCueing1 = StringBuffer(self.g13.parser, 0x7428   , 1, lambda s: self.setData(31,s))
-        self.bufferOptionCueing2 = StringBuffer(self.g13.parser, 0x742a   , 1, lambda s: self.setData(32,s))
-        self.bufferOptionCueing3 = StringBuffer(self.g13.parser, 0x742c   , 1, lambda s: self.setData(33,s))
-        self.bufferOptionCueing4 = StringBuffer(self.g13.parser, 0x742e   , 1, lambda s: self.setData(34,s))
-        self.bufferOptionCueing5 = StringBuffer(self.g13.parser, 0x7430   , 1, lambda s: self.setData(35,s))
-        self.bufferFuelTotal = StringBuffer(self.g13.parser, 0x748a    , 6, lambda s: self.setData(40,s))
+        self.bufferScratchpadString1Display = StringBuffer(self.g13.parser, 0x744e, 2, lambda s: self.setData('ScratchpadString1Display',s))
+        self.bufferScratchpadString2Display = StringBuffer(self.g13.parser, 0x7450, 2, lambda s: self.setData('ScratchpadString2Display',s))
+        self.bufferScratchpadNumberDisplay = StringBuffer(self.g13.parser, 0x7446, 8, lambda s: self.setData('ScratchpadNumberDisplay',s))
+        self.bufferOptionDisplay1 = StringBuffer(self.g13.parser, 0x7432, 4, lambda s: self.setData('OptionDisplay1',s))
+        self.bufferOptionDisplay2 = StringBuffer(self.g13.parser, 0x7436, 4, lambda s: self.setData('OptionDisplay2',s))
+        self.bufferOptionDisplay3 = StringBuffer(self.g13.parser, 0x743a, 4, lambda s: self.setData('OptionDisplay3',s))
+        self.bufferOptionDisplay4 = StringBuffer(self.g13.parser, 0x743e, 4, lambda s: self.setData('OptionDisplay4',s))
+        self.bufferOptionDisplay5 = StringBuffer(self.g13.parser, 0x7442, 4, lambda s: self.setData('OptionDisplay5',s))
+        self.bufferCOMM1Display = StringBuffer(self.g13.parser, 0x7424, 2, lambda s: self.setData('COMM1Display',s))
+        self.bufferCOMM2Display = StringBuffer(self.g13.parser, 0x7426, 2, lambda s: self.setData('COMM2Display',s))
+        self.bufferOptionCueing1 = StringBuffer(self.g13.parser, 0x7428, 1, lambda s: self.setData('OptionCueing1',s))
+        self.bufferOptionCueing2 = StringBuffer(self.g13.parser, 0x742a, 1, lambda s: self.setData('OptionCueing2',s))
+        self.bufferOptionCueing3 = StringBuffer(self.g13.parser, 0x742c, 1, lambda s: self.setData('OptionCueing3',s))
+        self.bufferOptionCueing4 = StringBuffer(self.g13.parser, 0x742e, 1, lambda s: self.setData('OptionCueing4',s))
+        self.bufferOptionCueing5 = StringBuffer(self.g13.parser, 0x7430, 1, lambda s: self.setData('OptionCueing5',s))
+        self.bufferFuelTotal = StringBuffer(self.g13.parser, 0x748a, 6, lambda s: self.setData('FuelTotal',s))
 
     def updateDisplay(self):
         super(FA18Handler, self).updateDisplay()
@@ -78,53 +78,6 @@ class FA18Handler(AircraftHandler):
         self.g13.updateDisplay(self.g13.img)
         
     def setData(self, selector, value, update=True):
-		#programming noob here, but it's pretty clear how to use this monster
-        if	selector==1:
-        	modifiedString=value
-        	modifiedString=modifiedString.replace('`','1')
-        	modifiedString=modifiedString.replace('~','2')
-        	self.ScratchpadString1Display=modifiedString
-        elif	selector==2:
-        	modifiedString=value
-        	modifiedString=modifiedString.replace('`','1')
-        	modifiedString=modifiedString.replace('~','2')
-        	self.ScratchpadString2Display=modifiedString
-        elif	selector==3:
-        	self.ScratchpadNumberDisplay=value
-        elif	selector==11:
-        	self.OptionDisplay1=value
-        elif	selector==12:
-        	self.OptionDisplay2=value
-        elif	selector==13:
-        	self.OptionDisplay3=value
-        elif	selector==14:
-        	self.OptionDisplay4=value
-        elif	selector==15:
-        	self.OptionDisplay5=value
-        elif	selector==21:
-        	# for unknown reason dcs_bios returns symbols instead '1' and '2' from comm channel display, so here we can correct this
-        	modifiedString=value
-        	modifiedString=modifiedString.replace('`','1')
-        	modifiedString=modifiedString.replace('~','2')
-        	self.COMM1Display=modifiedString
-        elif	selector==22:
-        	modifiedString=value
-        	modifiedString=modifiedString.replace('`','1')
-        	modifiedString=modifiedString.replace('~','2')
-        	self.COMM2Display=modifiedString
-        elif	selector==31:
-        	self.OptionCueing1=value
-        elif	selector==32:
-        	self.OptionCueing2=value
-        elif	selector==33:
-        	self.OptionCueing3=value
-        elif	selector==34:
-        	self.OptionCueing4=value	
-        elif	selector==35:
-        	self.OptionCueing5=value	
-        elif	selector==40:
-        	self.FuelTotal=value
-        else:
-        	print("No such selector: ", selector)
-        if update:
-        	self.updateDisplay()
+        if selector in ('ScratchpadString1Display', 'ScratchpadString2Display', 'COMM1Display', 'COMM2Display'):
+            value = value.replace('`', '1').replace('~', '2')
+        super().setData(selector, value, update)
